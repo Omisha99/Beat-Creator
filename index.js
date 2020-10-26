7a7cea4c959439b35e69e5ce6445f254de740833
omisha99 <omisha.sapra@gmail.com>
2020-10-27 00:50:22 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#18FFFF",
+        "#C6FF00",
+        "#ff1c67",
+        "#fffb00",
+        "#8d2acf",
+        "#ff8800"
+    ];
+
+    //Sound here
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            bubbles(index);
+        });
+    });
+
+    //Create Bubbles
+    const bubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    };
+});
